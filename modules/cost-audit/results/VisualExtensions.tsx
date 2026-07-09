68d8a17036c03f2615eebab3307605c0ca31baa8
Ayush <ayushbunkar100@gmail.com>
2026-07-10 00:48:44 +0530

    kjhgk

--- modules/cost-audit/results/VisualExtensions.tsx
+++ modules/cost-audit/results/VisualExtensions.tsx
@@ -221,6 +221,5 @@ export const OptimizationOpportunities = () => {
           </div>
         </div>
       </div>
-    </div>
   );
 };
--- modules/opportunity-audit/results/OpportunityVisualExtensions.tsx
+++ modules/opportunity-audit/results/OpportunityVisualExtensions.tsx
@@ -229,6 +229,5 @@ export const GrowthOpportunities = () => {
           </div>
         </div>
       </div>
-    </div>
   );
 };
